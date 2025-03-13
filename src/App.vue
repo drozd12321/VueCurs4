db1fd5d97169f8801118a312f222cfa4f993aded
Drozdov Evgeny <drozdove912@gmail.com>
2025-03-13 11:49:22 +0200

    data

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<script>
+import AppPeople from './components/AppPeople.vue'
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      name: '',
+      people: [],
+    }
+  },
+  components: {
+    AppPeople,
+  },
+  methods: {
+    async personSubmit() {
+      console.log(this.name)
+      const resp = await fetch(
+        'https://vuehttp-6614a-default-rtdb.europe-west1.firebasedatabase.app/people.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            firstName: this.name,
+          }),
+        },
+      )
+      const fireBase = await resp.json()
+      console.log(fireBase)
+      this.name = ''
+    },
+    async loadData() {
+      console.log('sdd')
+      const { data } = await axios.get(
+        'https://vuehttp-6614a-default-rtdb.europe-west1.firebasedatabase.app/people.json',
+      )
+      const pipl = Object.keys(data).map((item) => {
+        return {
+          id: item,
+          firstname: data[item].firstName,
+        }
+      })
+
+      this.people = pipl
+      console.log(this.people)
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="form" @submit.prevent="personSubmit">
+    <form class="fr">
+      <h1>Работа с БД</h1>
+      <div class="inp">
+        <label for="name">Имя:</label>
+        <input type="text" id="name" v-model.trim="name" />
+      </div>
+      <div class="inp">
+        <button class="btn" :disabled="name.length === 0">Отправить</button>
+      </div>
+    </form>
+    <AppPeople :people="people" @load="loadData"></AppPeople>
+  </div>
+</template>
+
+<style scoped>
+.inp {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.form {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+.fr {
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.4);
+  background-color: white;
+  width: 60%;
+  border-radius: 10px;
+  height: auto;
+  padding: 10px;
+}
+.fr h1 {
+  text-align: center;
+}
+.btn {
+  background-color: black;
+  color: white;
+  padding: 6px;
+  margin-top: 7px;
+  border-radius: 6px;
+  font-size: 20px;
+  border: none;
+}
+input {
+  outline: none;
+  border-radius: 6px;
+  margin-left: 4px;
+  background-color: rgb(92, 89, 89);
+  border: none;
+  color: white;
+  font-size: 15px;
+  padding: 5px;
+}
+button:disabled {
+  background-color: rgb(92, 89, 89);
+}
+</style>
---
+++ src/components/AppPeople.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="cont" v-if="people.length > 0">
+    <div class="inf" v-for="pipl in people">
+      <span>{{ pipl.id }}:&nbsp;</span>
+      <span
+        ><strong>{{ pipl.firstname }}</strong></span
+      >
+    </div>
+  </div>
+  <div class="cont" v-else>
+    <div class="inf">
+      <h3>Нет данных</h3>
+      <button class="btn" @click="$emit('load')">Загрузить данные</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  emits: ['load'],
+  props: ['people'],
+}
+</script>
+<style>
+.cont {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 70%;
+  height: auto;
+}
+.inf {
+  background-color: rgb(255, 255, 255);
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.4);
+  text-align: center;
+  padding: 7px;
+}
+.btn {
+  background-color: black;
+  color: white;
+  padding: 6px;
+  margin-top: 7px;
+  border-radius: 6px;
+  font-size: 20px;
+  border: none;
+}
+</style>
